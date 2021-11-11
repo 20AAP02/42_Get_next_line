bcd53ce597841ab88d72412309fbd7a3cac30e0f
Antonio Maria mouro ferreira abranches pinto <amaria-m@c1r14s6.42lisboa.com>
2021-11-11 15:25:24 +0000

    a

--- get_next_line.c
+++ get_next_line.c
@@ -2,6 +2,18 @@
 
 #define BUFFER_SIZE 40
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
@@ -24,6 +36,54 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	return (ft_strlen(dst) + ft_strlen(&src[j]));
 }
 
+char	*ft_strdup(const char *s1)
+{
+	int		s1_lenght;
+	char	*str;
+	int		i;
+
+	s1_lenght = 0;
+	while (s1[s1_lenght])
+		s1_lenght++;
+	str = malloc(s1_lenght + 1);
+	if (!str)
+		return (str);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+		i++;
+	j = 0;
+	while (s2[j])
+		j++;
+	new_str = malloc(i + j + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		new_str[i++] = *s1++;
+	while (*s2)
+		new_str[i++] = *s2++;
+	new_str[i] = 0;
+	return (new_str);
+}
+
 char	*ft_strchr(const char *s, int c)
 {
 	int	index;
@@ -37,22 +97,103 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	index;
+
+	if (!s)
+		return (NULL);
+	if (!len || ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (ft_strlen(s) - start > len)
+		ptr = (char *)malloc((len + 1) * sizeof(char));
+	else
+		ptr = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	index = 0;
+	while (index < len && s[start + index])
+	{
+		ptr[index] = s[start + index];
+		index++;
+	}
+	ptr[index] = '\0';
+	return (ptr);
+}
+
+char *get_line(char *line, char *backup)
+{
+	int i;
+	char *ptr_free;
+	if (ft_strchr(backup, '\n') != NULL)
+		line = malloc((ft_strchr(backup, '\n') - backup) + 2);
+	else
+		line = malloc((ft_strchr(backup, '\0') - backup) + 1);
+	i = 0;
+	while(backup[i])
+	{
+		if (backup[i] == '\n')
+		{
+			line[i] = '\n';
+			line[i + 1] = 0;
+			break;
+		}
+		else if (backup[i] == '\0')
+		{
+			line[i] = '\0';
+			free(backup);
+			backup = NULL;
+			break;
+		}
+		else
+		{
+			line[i] = backup[i];
+			i++;
+		}
+	}
+	return (line);
+}
+
+void delete_line_from_backup(char *backup, char *line)
+{
+	if (ft_strchr(backup, '\n') != NULL)
+		backup = ft_strchr(backup, '\n') + 1;
+	else
+	{
+		free(backup);
+		backup = NULL;
+	}	
+}
+
 char *get_next_line(int fd)
 {
-	char buf[BUFFER_SIZE];
+	char buf[BUFFER_SIZE + 1];
 	static char *backup;
+	unsigned long bytes_read;
+	char *line;
 
 	if (fd < 0 || fd >= RLIMIT_NOFILE || BUFFER_SIZE < 1)
 		return (NULL);
-	if (backup != NULL && ft_strchr(backup, '\n') == NULL)
-		backup = NULL;
-	while (ft_strchr(backup, '\n') == NULL)
+	bytes_read = BUFFER_SIZE;
+	if (backup == NULL)
 	{
-		read(fd, buf, BUFFER_SIZE);
-		strlcat(backup, buf, BUFFER_SIZE);
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read == 0)
+			return (NULL);
+		backup = malloc(1);
+		*backup = 0;
+		backup = ft_strjoin(backup, buf);
 	}
-	if (ft_strchr(backup, '\n') == )
-	return (NULL);
+	while (ft_strchr(backup, '\n') == NULL && bytes_read == BUFFER_SIZE)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		buf[bytes_read] = 0;
+		backup = ft_strjoin(backup, buf);
+	}
+	line = get_line(line, backup);
+	delete_line_from_backup(backup, line);
+	return (line);
 }
 
 int main()
